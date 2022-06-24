c5dcf459a98269ccbdebed216cbe1604e7e464e4
Danny Wade <danny.lee.wade@gmail.com>
2022-06-24 09:51:16 +0100

    Initial Commit

---
+++ GitTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitTest
+//
+//  Created by Danny Wade on 24/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.orange.ignoresSafeArea()
+            
+            Text("Hello, Git!")
+                .foregroundColor(.white)
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Danny Wade on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
